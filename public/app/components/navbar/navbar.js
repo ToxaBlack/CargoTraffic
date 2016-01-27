42360d3f36880ca4d4554b96644d3dfd2e540c3b
ToxaBlack <toxablack@gmail.com>
2016-01-27 20:38:22 +0300

    Make password and account page modal

--- public/app/components/navbar/navbar.js
+++ public/app/components/navbar/navbar.js
@@ -1,5 +1,5 @@
-define(['app/service/authService', 'app/service/navService', "knockout", "text!./navbar.html"],
-    function (authService, navService, ko, navbarTemplate) {
+define(['app/service/authService', 'app/service/navService', 'app/service/accountService', "knockout", "text!./navbar.html"],
+    function (authService, navService, accountService, ko, navbarTemplate) {
         "use strict";
 
         function navbarViewModel() {
@@ -7,7 +7,7 @@ define(['app/service/authService', 'app/service/navService', "knockout", "text!.
 
             self.mainPage = function () {
                 navService.mainPage();
-            }
+            };
 
             self.logout = function () {
                 authService.logout(
@@ -18,8 +18,69 @@ define(['app/service/authService', 'app/service/navService', "knockout", "text!.
                     })
             };
 
+
+            self.oldPassword = ko.observable();
+            self.newPassword = ko.observable();
+            self.confirmPassword = ko.observable();
+            self.error = ko.observable();
+
+            self.updatePassword = function () {
+                if (self.newPassword() === self.confirmPassword())
+                    accountService.updatePassword(self.oldPassword(), self.newPassword(),
+                        function (data) {
+                            navService.mainPage();
+                        },
+                        function (data) {
+                            switch (data.status) {
+                                case 400:
+                                    self.error(data.responseText);
+                                    break;
+                                case 403:
+                                    navService.navigateTo("login");
+                                    break;
+                                default:
+                                    navService.navigateTo("error");
+                            }
+                        });
+                else self.error("Passwords don't match");
+            };
+
+            self.account = ko.observable({});
+            self.update = function () {
+                accountService.updateAccount(self.account(),
+                    function (data) {
+                        navService.mainPage();
+                    },
+                    function (data) {
+                        switch (data.status) {
+                            case 403:
+                                navService.navigateTo("login");
+                                break;
+                            default:
+                                navService.navigateTo("error");
+                        }
+                    });
+            };
+
+            accountService.get(
+                function (data) {
+                    self.account(data);
+                },
+                function (data) {
+                    switch (data.status) {
+                        case 403:
+                            navService.navigateTo("login");
+                            break;
+                        default:
+                            navService.navigateTo("error");
+                    }
+                });
+
+
             return self;
         }
+
+
 
         return {viewModel: navbarViewModel, template: navbarTemplate};
     });
