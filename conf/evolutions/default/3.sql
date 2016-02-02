be62751d1a6dd481bd4b23742d4c91e8150bbb9f
Dmitriy <sankodmitriy@mail.ru>
2016-02-02 22:54:07 +0300

    updated SQL scripts

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -3,31 +3,29 @@
 # --- !Ups
 
 CREATE TABLE IF NOT EXISTS `cargo_traffic`.`packing_list` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `number` VARCHAR(250) NOT NULL,
   `issue_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `dispatcher` INT(11) NOT NULL,
+  `dispatcher` INT(11) UNSIGNED NOT NULL,
   `status` VARCHAR(250) NULL DEFAULT NULL,
-  `company_id` INT(11) NOT NULL,
+  `company_id` INT(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_dispatcher_id` (`dispatcher` ASC),
-  INDEX `fk_company_id` (`company_id` ASC),
-  CONSTRAINT `fk_dispatcher`
+  INDEX (`dispatcher` ASC),
+  INDEX (`company_id` ASC),
   FOREIGN KEY (`dispatcher`)
   REFERENCES `cargo_traffic`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_company`
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
   FOREIGN KEY (`company_id`)
   REFERENCES `cargo_traffic`.`company` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT)
   ENGINE = InnoDB
   DEFAULT CHARACTER SET = utf8mb4;
 
 
 CREATE TABLE IF NOT EXISTS `cargo_traffic`.`vehicle_type` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(150) NOT NULL,
   PRIMARY KEY (`id`))
   ENGINE = InnoDB
@@ -35,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `cargo_traffic`.`vehicle_type` (
 
 
 CREATE TABLE IF NOT EXISTS `cargo_traffic`.`vehicle_fuel` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(255) NOT NULL,
   `cost` DECIMAL(10,2) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`))
@@ -44,103 +42,94 @@ CREATE TABLE IF NOT EXISTS `cargo_traffic`.`vehicle_fuel` (
 
 
 CREATE TABLE IF NOT EXISTS `cargo_traffic`.`vehicle` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `vehicle_producer` VARCHAR(250) NOT NULL,
+  `vehicle_model` VARCHAR(250) NOT NULL,
   `license_plate` VARCHAR(250) NOT NULL,
-  `products_weight` DOUBLE(10,2) NOT NULL,
-  `fuel_consumption` DOUBLE(10,2) NOT NULL,
-  `company_id` INT(11) NOT NULL,
-  `vehicle_type_id` INT(11) NOT NULL,
-  `vehicle_fuel_id` INT(11) NOT NULL,
+  `products_weight` DOUBLE(10,2) UNSIGNED NOT NULL,
+  `fuel_consumption` DOUBLE(10,2) UNSIGNED NOT NULL,
+  `company_id` INT(11) UNSIGNED NOT NULL,
+  `vehicle_type_id` INT(11) UNSIGNED NOT NULL,
+  `vehicle_fuel_id` INT(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_company_id` (`company_id` ASC),
-  INDEX `fk_vehicle_type_id` (`vehicle_type_id` ASC),
-  INDEX `fk_vehicle_fuel_id` (`vehicle_fuel_id` ASC),
-  CONSTRAINT `fk_vehicle_company1`
+  INDEX (`company_id` ASC),
+  INDEX (`vehicle_type_id` ASC),
+  INDEX (`vehicle_fuel_id` ASC),
   FOREIGN KEY (`company_id`)
   REFERENCES `cargo_traffic`.`company` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_vehicle_vehicle_type1`
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
   FOREIGN KEY (`vehicle_type_id`)
   REFERENCES `cargo_traffic`.`vehicle_type` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_vehicle_vehicle_fuel1`
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
   FOREIGN KEY (`vehicle_fuel_id`)
   REFERENCES `cargo_traffic`.`vehicle_fuel` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT)
   ENGINE = InnoDB
   DEFAULT CHARACTER SET = utf8mb4;
 
 
 CREATE TABLE IF NOT EXISTS `cargo_traffic`.`waybill` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
   `issue_date` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `departure_date` TIMESTAMP NULL DEFAULT NULL,
   `status` VARCHAR(250) NOT NULL,
-  `departure_warehouse` INT(11) NOT NULL,
-  `destination_warehouse` INT(11) NOT NULL,
-  `packing_list_id` INT(11) NOT NULL,
-  `manager` INT(11) NOT NULL,
-  `company_id` INT(11) NOT NULL,
+  `departure_warehouse` INT(11) UNSIGNED NOT NULL,
+  `destination_warehouse` INT(11) UNSIGNED NOT NULL,
+  `packing_list_id` INT(11) UNSIGNED NOT NULL,
+  `manager` INT(11) UNSIGNED NOT NULL,
+  `company_id` INT(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_departure_warehouse_id` (`departure_warehouse` ASC),
-  INDEX `fk_destination_warehouse_id` (`destination_warehouse` ASC),
-  INDEX `fk_packing_list_id` (`packing_list_id` ASC),
-  INDEX `fk_manager_id` (`manager` ASC),
-  INDEX `fk_company_id` (`company_id` ASC),
-  CONSTRAINT `fk_packing_list`
+  INDEX (`departure_warehouse` ASC),
+  INDEX (`destination_warehouse` ASC),
+  INDEX (`packing_list_id` ASC),
+  INDEX (`manager` ASC),
+  INDEX (`company_id` ASC),
   FOREIGN KEY (`packing_list_id`)
   REFERENCES `cargo_traffic`.`packing_list` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_departure_warehouse`
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
   FOREIGN KEY (`departure_warehouse`)
   REFERENCES `cargo_traffic`.`warehouse` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_destination_warehouse`
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
   FOREIGN KEY (`destination_warehouse`)
   REFERENCES `cargo_traffic`.`warehouse` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_manager`
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
   FOREIGN KEY (`manager`)
   REFERENCES `cargo_traffic`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_company_id`
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
   FOREIGN KEY (`company_id`)
   REFERENCES `cargo_traffic`.`company` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT)
   ENGINE = InnoDB
   DEFAULT CHARACTER SET = utf8mb4;
 
 
 CREATE TABLE IF NOT EXISTS `cargo_traffic`.`waybill_vehicle_driver` (
-  `vehicle_id` INT(11) NOT NULL,
-  `driver_id` INT(11) NOT NULL,
-  `waybill_id` INT(11) NOT NULL,
+  `vehicle_id` INT(11) UNSIGNED NOT NULL,
+  `driver_id` INT(11) UNSIGNED NOT NULL,
+  `waybill_id` INT(11) UNSIGNED NOT NULL,
   PRIMARY KEY (`vehicle_id`, `driver_id`, `waybill_id`),
-  INDEX `fk_user_id` (`driver_id` ASC),
-  INDEX `fk_waybill_id` (`waybill_id` ASC),
-  CONSTRAINT `fk_vehicle`
+  INDEX (`driver_id` ASC),
+  INDEX (`waybill_id` ASC),
   FOREIGN KEY (`vehicle_id`)
   REFERENCES `cargo_traffic`.`vehicle` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_driver`
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
   FOREIGN KEY (`driver_id`)
   REFERENCES `cargo_traffic`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_waybill`
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT,
   FOREIGN KEY (`waybill_id`)
   REFERENCES `cargo_traffic`.`waybill` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+    ON DELETE RESTRICT
+    ON UPDATE RESTRICT)
   ENGINE = InnoDB
   DEFAULT CHARACTER SET = utf8mb4;
 
